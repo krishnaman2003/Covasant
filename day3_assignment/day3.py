e4dea35d1cdd002b6b1216716ab1c822fb540011
krishnaman2003 <krishnaman2003@gmail.com>
2025-04-08 22:24:40 +0530

    initial commit

---
+++ day3_assignment/day3.py
@@ -0,0 +1,13 @@
+def convert(x):
+    if isinstance(x, list):
+        return [convert(element) for element in x]
+    elif isinstance(x, str):
+        string_list = x.strip('()').split(',')
+        # Convert the list of strings to a list of integers
+        return [int(s) for s in string_list]
+    else:
+        return x
+        
+input_list = [[['(0,1,2)', '(3,4,5)'], ['(5,6,7)', '(9,4,2)']]]
+output_list = convert(input_list)
+print(output_list)

9d07342919f93a33939341e7d278ed07ee99daa2
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-09 21:42:48 +0530

    initial commit

--- day1_assignment/q2day1.py
+++ day1_assignment/q2day1.py
@@ -1,17 +1,19 @@
-D1 = {'ok': 1, 'nok': 2}
-D2 = {'ok': 2, 'new': 3}
-D_UNION = D1.copy()  # Start with all keys from D1.
-for key, value in D2.items():
-    if key not in D_UNION:
-        D_UNION[key] = value
+d1 = {'ok': 1, 'nok': 2}
+d2 = {'ok': 2, 'new': 3}
 
-D_INTERSECTION = {key: D1[key] for key in D1 if key in D2}
-D_DIFFERENCE = {key: D1[key] for key in D1 if key not in D2}
-D_MERGE = {}
-for key in set(D1) | set(D2):
-    D_MERGE[key] = D1.get(key, 0) + D2.get(key, 0)
+d_union = d1.copy()
+for key, val in d2.items():
+    if key not in d_union:
+        d_union[key] = val
+print(f"D_UNION = {d_union}")
 
-print("D_UNION =", D_UNION)
-print("D_INTERSECTION =", D_INTERSECTION)
-print("D1 - D2 =", D_DIFFERENCE)
-print("D_MERGE =", D_MERGE)
+d_intersection = {key: d1[key] for key in d1 if key in d2}
+print(f"D_INTERSECTION = {d_intersection}")
+
+d_difference = {key: d1[key] for key in d1 if key not in d2}
+print(f"D1 - D2 = {d_difference}")
+
+d_merge = d1.copy()
+for key, val in d2.items():
+    d_merge[key] = d_merge.get(key, 0) + val
+print(f"D_MERGE = {d_merge}")

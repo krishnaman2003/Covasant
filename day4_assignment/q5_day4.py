312ca7b2f57ffbdb670694510daf2984001c5523
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-13 23:43:16 +0530

    initial commit

--- day4_assignment/q5_day4.py
+++ day4_assignment/q5_day4.py
@@ -1,3 +1,12 @@
+"""
+Question-5:
+from pkg.poly import Poly 
+a = Poly(1,2,3)  #an, ...., a0 
+b = Poly(1,0,1,1,2,3)
+c = a+b 
+print(c) #Poly ( 1,0,1, 2,4,6)
+"""
+
 from pkg.poly import Poly
 a = Poly(1, 2, 3)
 b = Poly(1, 0, 1, 1, 2, 3)

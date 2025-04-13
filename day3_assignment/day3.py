f1217d7e9f8cc4647f493e161dcadde477bceb94
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-13 23:41:21 +0530

    initial commit

--- day3_assignment/day3.py
+++ day3_assignment/day3.py
@@ -1,3 +1,13 @@
+"""
+flatten(lst)        Flattens the list 
+                    ie input = [1,2,3, [1,2,3,[3,4],2]]
+                    output = [1,2,3,1,2,3,3,4,2]
+Question-13              
+convert(x)          Converts like below 
+                    input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+                    output = [[[[0,1,2],[3,4,5]],[[5,6,7],[9,4,2]]]]
+"""
+
 def convert(x):
     if isinstance(x, list):
         return [convert(element) for element in x]

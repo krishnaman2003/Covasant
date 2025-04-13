ae4a64411de8e270e50c2d812096f3f628d5a5b6
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-13 23:44:27 +0530

    initial commit

--- day4_assignment/q6_day4.py
+++ day4_assignment/q6_day4.py
@@ -1,3 +1,13 @@
+"""
+Question-6:
+#MaxFile class 
+from pkg.file import File 
+fs = File(".")
+fs.getMaxSizeFile(2) # gives two max file names 
+fs.getLatestFiles(datetime.date(2018,2,1))
+#Returns list of files after 1st Feb 2018 
+"""
+
 from pkg.file import File
 import datetime
 fs = File(".")

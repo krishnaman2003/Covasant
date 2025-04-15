2a3caec5419de8bb87b828682759f501b95e1c17
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-15 21:36:01 +0530

    initial commit

--- day3_assignment/convert.py
+++ day3_assignment/convert.py
@@ -1,3 +1,10 @@
+"""
+Question-13              
+convert(x)          Converts like below 
+                    input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+                    output = [[[[0,1,2],[3,4,5]],[[5,6,7],[9,4,2]]]]
+"""
+
 def convert(data):
     if type(data) is list:
         return [convert(item) for item in data]

755178efaec6be0cdb829075fa69b7417980f854
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-15 21:31:15 +0530

    initial commit

--- day3_assignment/day3.py
+++ day3_assignment/day3.py
@@ -1,22 +1,19 @@
 """
-flatten(lst)        Flattens the list 
-                    ie input = [1,2,3, [1,2,3,[3,4],2]]
-                    output = [1,2,3,1,2,3,3,4,2]
 Question-13              
 convert(x)          Converts like below 
                     input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
                     output = [[[[0,1,2],[3,4,5]],[[5,6,7],[9,4,2]]]]
 """
 
-def convert(x):
-    if isinstance(x, list):
-        return [convert(element) for element in x]
-    elif isinstance(x, str):
-        string_list = x.strip('()').split(',')
-        return [int(s) for s in string_list]
+def convert(data):
+    if type(data) is list:
+        return [convert(item) for item in data]
+    elif type(data):
+        string_list = data.strip('()').split(',')
+        return [int(i) for i in string_list]
     else:
-        return x
+        return data
         
-input_list = [[['(0,1,2)', '(3,4,5)'], ['(5,6,7)', '(9,4,2)']]]
-output_list = convert(input_list)
-print(output_list)
+input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+output = convert(input)
+print(f"Output: {output}")

50cef679ec0bb3d906b4cd3b94a40c3abeb3af41
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-15 21:34:50 +0530

    Add files via upload

---
+++ day3_assignment/convert.py
@@ -0,0 +1,12 @@
+def convert(data):
+    if type(data) is list:
+        return [convert(item) for item in data]
+    elif type(data):
+        string_list = data.strip('()').split(',')
+        return [int(i) for i in string_list]
+    else:
+        return data
+        
+input = [[[ '(0,1,2)' , '(3,4,5)'], ['(5,6,7)' , '(9,4,2)']]]
+output = convert(input)
+print(f"Output: {output}")

03e2350b580b507bd0cc91cdc2e07d47d4b59b4f
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-20 20:39:31 +0530

    initial commit

--- day7_assignment/q16_day7.py
+++ day7_assignment/q16_day7.py
@@ -1,3 +1,11 @@
+"""
+Question-16:
+Sharing of content 
+@app.route("/updatefortoday", methods=['GET','POST'])#http://localhost:5000/updatefortoday
+@app.route("/share", methods=['GET'])#http://localhost:5000/share
+@app.route("/clearnotepadtxt", methods=['GET'])#http://localhost:5000/clearnotepadtxt
+"""
+
 from flask import Flask, request
 import os
 

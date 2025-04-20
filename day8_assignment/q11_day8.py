1e785975026baee3d3027a3c2101cc84b036111e
Naman Krishn Raj T <107807399+krishnaman2003@users.noreply.github.com>
2025-04-20 20:41:11 +0530

    initial commit

--- day8_assignment/q11_day8.py
+++ day8_assignment/q11_day8.py
@@ -1,3 +1,18 @@
+"""
+Question-11:
+Weather mock API 
+format comes from below endpoint 
+    Parse format, if xml, show xml, 
+    if none, show json 
+    Put all random temp 
+    City name may change based on PATH param 
+    (currently not implemented in demo api endpoint)
+
+DO the same as 
+http://notepadfromdas.pythonanywhere.com/w/mumbai?format=xml
+http://notepadfromdas.pythonanywhere.com/w/mumbai
+"""
+
 from flask import Flask, request, jsonify, make_response
 import json, os, random
 
